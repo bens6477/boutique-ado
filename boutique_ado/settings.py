8f65af3c5e731691846c8c8f21f33d9a38840065
Ben Smith <bens6477@gmail.com>
2022-11-26 12:24:38 +0000

    Added secret key for BA

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#1ror3ia3cup4+^o8n++wis$%va@8rc9l5xg6jqa2vs7ewj2li'
+SECRET_KEY = os.environ.get('SECRET_KEY_BA', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'Development' in os.environ
